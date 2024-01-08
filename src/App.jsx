d0f6bf48e159da1c63251f2370f96e7352e5ba4d
jlearns9 <142127358+jlearns9@users.noreply.github.com>
2024-01-07 22:58:07 -0600

    Added Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 // import React from 'react'
-
+import Header from "./components/Header";
 export default function App() {
-
-  return (
-    <>
-      <h1>jogo.is_</h1>
-    </>
-  )
+    return (
+        <>
+            <Header />
+        </>
+    );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <>
+            <div className="header">
+                <h1 className="header__title">JOGO</h1>
+                <hr className="header__border" />
+            </div>
+        </>
+    )
+}
