51cc43fa0844e3e78f171b976c12c49042977596
jlearns9 <142127358+jlearns9@users.noreply.github.com>
2024-01-11 02:22:48 -0600

    Added about section

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+    return (
+        <>
+            <h2>WELCOME...</h2>
+            <p>
+                MY NAME IS JORGE GOMEZ AND I AM A{" "}
+                <span className="orangered">DESIGNER</span> /{" "}
+                <span className="green">DEVELOPER</span> WITH A PASSION TO
+                CREATE EFFECTIVE AND BEAUTIFUL WEB APPLICATIONS.
+            </p>
+        </>
+    );
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,9 @@
+import About from "./About"
+
 export default function Main() {
     return (
         <>
-            <h2>WELCOME...</h2>
-            <p>
-                MY NAME IS JORGE GOMEZ AND I'M A{" "}
-                <span className="orangered">DESIGNER</span> /{" "}
-                <span className="green">DEVELOPER</span> WITH A PASSION TO CREATE EFFECTIVE AND
-                BEAUTIFUL WEB APPLICATIONS
-            </p>
+            <About />
         </>
     );
 }
