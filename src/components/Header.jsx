85ec1f7fa8f43263c098aaf243277f8d3a571224
jlearns9 <142127358+jlearns9@users.noreply.github.com>
2024-01-11 00:49:48 -0600

    small update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import TextScramble from "./TextScramble"
 
 export default function Header() {
 
-    const words = ['DEVELOPER /', 'DESIGNER /', 'ENGINEER /', 'OCCASIONAL CHEF /'];
+    const words = ['DEVELOPER /', 'DESIGNER /', 'CODER /', 'ENGINEER /', 'OCCASIONAL CHEF /'];
     
     return (
         <>
             <div className="header">
                 <div className="header__details flex">
                     <h1 className="header__title">&lt;<TextScramble words={words} />&gt;</h1>
-                    <h1>JOGO</h1>
+                    <h1 className="header__name">JOGO</h1>
                 </div>
                 <hr className="header__border" />
             </div>
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+export default function Main() {
+    return (
+        <>
+            <h2>WELCOME...</h2>
+            <p>
+                MY NAME IS JORGE GOMEZ AND I'M A{" "}
+                <span className="orangered">DESIGNER</span> /{" "}
+                <span className="green">DEVELOPER</span> WITH A PASSION TO CREATE EFFECTIVE AND
+                BEAUTIFUL WEB APPLICATIONS
+            </p>
+        </>
+    );
+}
