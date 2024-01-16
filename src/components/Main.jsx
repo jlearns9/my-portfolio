4a24041e647f9257a5e5861b8513c881ebf3e023
jlearns9 <142127358+jlearns9@users.noreply.github.com>
2024-01-16 17:53:33 -0600

    uPdate

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,11 @@
 import About from "./About"
+import Skills from "./Skills"
 
 export default function Main() {
     return (
-        <>
+        <div className="flex">
             <About />
-        </>
+            <Skills />
+        </div>
     );
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { FaHtml5 } from "react-icons/fa";
+import { SiJavascript } from "react-icons/si";
+import { SiCss3 } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+
+export default function Skills() {
+    const [stats, setStats] = useState(null);
+
+    useEffect(() => {
+        fetch("https://www.codewars.com/api/v1/users/jlearns9")
+            .then((res) => res.json())
+            .then((data) => setStats(data));
+    }, []);
+
+    return (
+        <div className="skills">
+            <h2 className="skills__title">Skills</h2>
+            <ul className="skills__list">
+                <li className="list__item flex">
+                    <div className="color-primary item-logo">
+                        <FaHtml5 />
+                    </div>{" "}
+                    <div>HTML</div>
+                </li>
+                <li className="list__item flex">
+                    <div className="color-blue item-logo">
+                        <SiCss3 />
+                    </div>{" "}
+                    <div>CSS</div>
+                </li>
+                <li className="list__item flex">
+                    <div className="color-yellow item-logo">
+                        <SiJavascript />
+                    </div>{" "}
+                    <div>JAVASCRIPT</div>
+                </li>
+                <li className="list__item flex">
+                    <div className="color-light-blue item-logo">
+                        <FaReact />
+                    </div>{" "}
+                    <div>REACT</div>
+                </li>
+            </ul>
+            {/* {stats && <h3>Username: {stats.username}</h3>} */}
+        </div>
+    );
+}
