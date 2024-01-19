09506153d4409e732780800fd37a51a84655f567
jlearns9 <142127358+jlearns9@users.noreply.github.com>
2024-01-18 21:13:01 -0600

    update

--- src/components/Project/ProjectImage.jsx
+++ src/components/Project/ProjectImage.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
 
-export default function ProjectImage({ src }) {
+export default function ProjectImage() {
 
     return (
-        <div>
+        <>
             <img className="project__image" src="../../images/component.png" alt="project preview" />
-        </div>
+        </>
     );
 }
