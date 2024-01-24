e7324335d0d02a53a497d8879bac5c67d3d9332b
jlearns9 <142127358+jlearns9@users.noreply.github.com>
2024-01-23 22:25:48 -0600

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,56 +13,60 @@ export default function Main() {
                 <hr className="border" />
                 <div className="projects">
                     <h2 className="projects__title">PROJECTS</h2>
-                    <Project>
-                        <div className="project__wide">
-                            <Project.Title
-                                gitLink="https://github.com/jlearns9/component-library"
-                                webLink="https://component-library-8yl.pages.dev/"
-                            >
-                                REACT COMPONENT LIBRARY
-                            </Project.Title>
-                            <Project.Image src="./component.png" />
-                        </div>
-                        <Project.Description>
-                            A versatile React component library that helps
-                            developers create and plan web applications with ease.
-                            You can customize and use these components as you wish.
-                        </Project.Description>
-                    </Project>
-                    <Project>
-                        <div className="project__wide">
-                            <Project.Title
-                                gitLink="https://github.com/jlearns9/quizzical"
-                                webLink="https://quizzical-mlg.pages.dev/"
-                            >
-                                QUIZZICAL
-                            </Project.Title>
-                            <Project.Image src="./quizzical.png" />
-                        </div>
-                        <Project.Description>
-                            Web application designed for quiz enthusiasts. It
-                            utilizes APIs to fetch quiz questions, allowing users to
-                            play a game of &ldquo;Quizzical&ldquo;
-                        </Project.Description>
-                    </Project>
-                    <Project>
-                         <div className="project__wide">
-                            <Project.Title
-                                gitLink="https://github.com/jlearns9/tenzies"
-                                webLink="https://tenzies-cmi.pages.dev/"
-                            >
-                                TENZIES
-                            </Project.Title>
-                            <Project.Image src="./tenzies.png" />
-                        </div>
-                        <Project.Description>
-                            Enjoy a digital game of Tenzies based off the fast-paced
-                            diced game that many take delight in. Built using HTML,
-                            CSS, JS & React, this project was aimed at not only
-                            recreating Tenzies but also enhancing it with digital
-                            conveniences.
-                        </Project.Description>
-                    </Project>
+                    <div className="wide__test">
+                        <Project>
+                            <div className="project__wide">
+                                <Project.Title
+                                    gitLink="https://github.com/jlearns9/component-library"
+                                    webLink="https://component-library-8yl.pages.dev/"
+                                >
+                                    REACT COMPONENT LIBRARY
+                                </Project.Title>
+                                <Project.Image src="./component.png" />
+                                <Project.Description>
+                                    A versatile React component library that helps
+                                    developers create and plan web applications with
+                                    ease. You can customize and use these components
+                                    as you wish.
+                                </Project.Description>
+                            </div>
+                        </Project>
+                        <Project>
+                            <div className="project__wide">
+                                <Project.Title
+                                    gitLink="https://github.com/jlearns9/quizzical"
+                                    webLink="https://quizzical-mlg.pages.dev/"
+                                >
+                                    QUIZZICAL
+                                </Project.Title>
+                                <Project.Image src="./quizzical.png" />
+                                <Project.Description>
+                                    Web application designed for quiz enthusiasts.
+                                    It utilizes APIs to fetch quiz questions,
+                                    allowing users to play a game of
+                                    &ldquo;Quizzical&ldquo;
+                                </Project.Description>
+                            </div>
+                        </Project>
+                        <Project>
+                            <div className="project__wide">
+                                <Project.Title
+                                    gitLink="https://github.com/jlearns9/tenzies"
+                                    webLink="https://tenzies-cmi.pages.dev/"
+                                >
+                                    TENZIES
+                                </Project.Title>
+                                <Project.Image src="./tenzies.png" />
+                                <Project.Description>
+                                    Enjoy a digital game of Tenzies based off the
+                                    fast-paced diced game that many take delight in.
+                                    Built using HTML, CSS, JS & React, this project
+                                    was aimed at not only recreating Tenzies but
+                                    also enhancing it with digital conveniences.
+                                </Project.Description>
+                            </div>
+                        </Project>
+                    </div>
                 </div>
             </div>
             <Footer />
--- src/components/Project/ProjectImage.jsx
+++ src/components/Project/ProjectImage.jsx
@@ -3,8 +3,8 @@
 export default function ProjectImage({ src }) {
 
     return (
-        <>
+        <div className="image__container">
             <img className="project__image" src={src} alt="project preview" />
-        </>
+        </div>
     );
 }
